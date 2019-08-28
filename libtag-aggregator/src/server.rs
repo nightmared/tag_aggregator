0ac3c297097602208b108122e0b5c8a17f2f42e9
Simon THOBY <git@nightmared.fr>
2019-08-28 11:53:56 +0200

    still not working, due to polling issues

--- libtag-aggregator/src/server.rs
+++ libtag-aggregator/src/server.rs
@@ -4,14 +4,17 @@ use std::convert::TryFrom;
 use std::net::ToSocketAddrs;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{RwLock, Arc};
+use std::io::Read;
 
 use futures::{future, Async, Future, Stream};
+use futures::future::IntoFuture;
 use serde_derive::{Serialize, Deserialize};
 use hyper::{Client, Body, Request, Response, Server, Method};
 use hyper::body::Payload;
 use hyper::service::service_fn;
 use bytes::{BytesMut, Buf, BufMut, IntoBuf};
 use tokio_codec::{Encoder, Decoder};
+use tokio_io::{AsyncRead, AsyncWrite};
 
 use macro_hack::try_future;
 
@@ -26,7 +29,7 @@ impl Decoder for MessageCodec {
 	type Item = Message;
 	type Error = crate::Error;
 
-	fn decode(self: &mut Self, buf: &mut BytesMut) -> Result<Option<Message>, crate::Error> {
+	fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Message>, crate::Error> {
 		if buf.len() > 8 {
 			let buf = &mut buf.clone().into_buf();
 			let size = buf.get_u64_be() as usize;
@@ -46,7 +49,7 @@ impl Encoder for MessageCodec {
 	type Item = Message;
 	type Error = crate::Error;
 
-	fn encode(self: &mut Self, msg: Message, buf: &mut BytesMut) -> Result<(), crate::Error> {
+	fn encode(&mut self, msg: Message, buf: &mut BytesMut) -> Result<(), crate::Error> {
 		let data = serde_json::to_vec(&msg)?;
 		let size = data.len();
 		buf.reserve(size+8);
@@ -83,6 +86,7 @@ impl Stream for ServerState {
 	type Error = std::io::Error;
 
 	fn poll(&mut self) -> Result<Async<Option<Self::Item>>, Self::Error> {
+        futures::task::current().notify();
 		if self.updated.compare_and_swap(true, false, Ordering::AcqRel) {
 			let serv_data = self.data.read().expect("RwLock starvation !");
 			let mut buf = bytes::BytesMut::new();
@@ -115,6 +119,7 @@ fn server_main(data: ServerState) -> impl Fn(Request<Body>) -> Box<dyn Future<It
 	move |req| {
 		match (req.method(), req.uri().path()) {
 			(&Method::GET, "/data.json") => {
+				println!("{:?}", data);
 				Box::new(future::ok(Response::builder()
 					.body(Body::wrap_stream(data.clone())).unwrap()))
 			},
@@ -125,7 +130,12 @@ fn server_main(data: ServerState) -> impl Fn(Request<Body>) -> Box<dyn Future<It
 					.map_err(|e| crate::Error::from(e))
 					.and_then(move |str| {
 						let body = try_future!(serde_json::from_slice(&str));
-						let serv_data = data.data.write().expect("RwLock starvation !");
+						{
+						let mut serv_data = data.data.write().expect("RwLock starvation !");
+						let last_version = if serv_data.len() > 0 { serv_data[serv_data.len()-1].0 } else { 1 };
+						serv_data.push((last_version, body));
+						}
+						println!("{:?}", data);
 						data.updated.store(true, Ordering::Release);
 						Box::new(future::ok(Response::builder()
 							.status(200)
@@ -185,30 +195,79 @@ pub fn run_web_server(config: ServerConfig, data: Arc<RwLock<ServerTree>>) {
 }
 
 struct MessageClient {
-	body: Body,
-	buf: BytesMut
+    buf: BytesMut,
+    fut: Box<dyn Stream<Item=hyper::Chunk, Error=hyper::Error> + Send>
 }
 
-impl Stream for MessageClient {
-	type Item = Message;
-	type Error = hyper::Error;
+//impl Stream for MessageClient {
+//	type Item = Message;
+//	type Error = hyper::Error;
+//
+//	fn poll(&mut self) -> Result<Async<Option<Message>>, hyper::Error> {
+//		println!("called !");
+//		match self.body.poll_data() {
+//			Ok(Async::Ready(Some(d))) => {
+//				let mut codec = MessageCodec;
+//				let src_bytes = d.into_bytes();
+//				self.buf.reserve(src_bytes.len());
+//				self.buf.put(src_bytes);
+//				match codec.decode(&mut self.buf) {
+//					Ok(Some(x)) => Ok(Async::Ready(Some(x))),
+//					_ => Ok(Async::NotReady)
+//				}
+//			},
+//			Err(e) => Err(e.into()),
+//			_ => Ok(Async::NotReady)
+//		}
+//	}
+//}
 
-	fn poll(self: &mut Self) -> Result<Async<Option<Message>>, hyper::Error> {
-		match self.body.poll_data() {
-			Ok(Async::Ready(Some(d))) => {
-				let mut codec = MessageCodec;
-				let src_bytes = d.into_bytes();
-				self.buf.reserve(src_bytes.len());
-				self.buf.put(src_bytes);
-				match codec.decode(&mut self.buf) {
-					Ok(Some(x)) => Ok(Async::Ready(Some(x))),
-					_ => Ok(Async::NotReady)
-				}
-			},
-			Err(e) => Err(e.into()),
-			_ => Ok(Async::NotReady)
-		}
+//impl std::io::Write for MessageClient {
+//	fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+//		self.buf.reserve(buf.len());
+//		self.buf.put(buf);
+//		Ok(buf.len())
+//	}
+//
+//	fn flush(&mut self) -> std::io::Result<()> {
+//		Ok(())
+//	}
+//}
+//
+//impl AsyncWrite for MessageClient {
+//	fn shutdown(&mut self) -> std::io::Result<Async<()>> {
+//		// dropping the body
+//		self.body = Body::default();
+//		Ok(Async::Ready(()))
+//	}
+//}
+
+impl std::io::Read for MessageClient {
+	fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize>  {
+		let offset = self.buf
+			.iter()
+			.zip(buf.iter_mut())
+			.map(|(&x, dest)| *dest = x)
+			.count();
+		self.buf.advance(offset);
+		Ok(offset)
 	}
+}
+
+impl AsyncRead for MessageClient {
+    fn poll_read(&mut self, buf: &mut [u8]) -> futures::Poll<usize, std::io::Error> {
+        println!("plled");
+        match self.fut.poll() {
+            Ok(Async::Ready(Some(chunk))) => {
+                let chunk = chunk.into_bytes();
+                self.buf.reserve(chunk.len());
+                self.buf.put(chunk);
+                Ok(Async::Ready(self.read(buf)?))
+            },
+            _ => Ok(Async::NotReady),
+            Err(e) => Err(crate::Error::from(e).into())
+        }
+    }
 }
 
 fn web_client(conn: Connection) -> Result<impl Future<Item=(), Error=()>, crate::Error> {
@@ -216,20 +275,24 @@ fn web_client(conn: Connection) -> Result<impl Future<Item=(), Error=()>, crate:
 	let dbus_path = dbus_conn.with_path("fr.nightmared.tag_aggregator", "/fr/nightmared/tag_aggregator", 500);
 
 	let client = Client::new();
-	let server_url = format!("http{}://{}:{}", if conn.use_tls { "s" } else { "" }, conn.server_name, conn.port);
+	let server_url = format!("http{}://{}:{}/data.json", if conn.use_tls { "s" } else { "" }, conn.server_name, conn.port);
 	let fut = client
 		.get(hyper::Uri::from_str(&server_url).unwrap())
 		.and_then(|res| {
-			MessageClient {
-				body: res.into_body(),
+            let frames = tokio_codec::FramedRead::new(MessageClient {
+				fut: Box::new(res.into_body()),
 				buf: BytesMut::new()
-			}
-			.for_each(|msg| {
+			}, MessageCodec);
+
+			Ok(frames.for_each(|msg| {
 				println!("{:?}", msg);
-				Ok(())
-			})
+                Err(crate::Error::IOError(std::io::Error::from(std::io::ErrorKind::AddrInUse)))
+				//Ok(())
+			}).into_future()
+            )
 		})
-		.map_err(|_| {});
+		.map_err(|e| {eprintln!("{:?}", e);})
+        .and_then(|_| Ok(()));
 	Ok(fut)
 }
 
